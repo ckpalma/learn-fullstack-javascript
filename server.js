bc288110f58d3d1be7aa236b2f3def5369db2ae4
Erick Palma <ckpalma@protonmail.com>
2019-11-21 17:31:06 -0800

    2.2 - http/https modules

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
-import config, { nodeEnv, logStars } from './config';
+import http from 'http';
 
-console.log(config, nodeEnv);
+const server = http.createServer((req, res) => {
+  res.write('Hello HTTP!\n');
+  setTimeout(() => {
+    res.write('I can stream!\n');
+    res.end();
+  }, 3000);
+});
 
-logStars('Function');
+server.listen(8080);
